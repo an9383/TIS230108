3069e68c10623e05a02e44a0d1e87ac90fee8247
an9383 <an9383@gmail.com>
2025-05-11 17:10:25 +0900

    all

---
+++ React/rt03.Webpack/webpackprj01/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div w3-include-header="header.html">
+        <header data-role="header">
+          <h1>Header.html</h1>
+        </header>
+      </div>
+      <section id="page1" data-role="page">
+        <div class="content" data-role="content">
+          컨텐츠
+        </div>
+      </section>
+      <div w3-include-footer="footer.html">
+        <footer data-role="footer">
+          <h1>Footer.html</h1>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
